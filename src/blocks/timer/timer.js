ee50c597d48843c0bbe198544291457c6004a532
Kupriyanova <kozina@morizo.ru>
2017-01-18 01:04:01 +0300

    'start_common_news_page'

--- src/blocks/timer/timer.js
+++ src/blocks/timer/timer.js
@@ -1,25 +1,30 @@
 /*nataly*/
 $(function () {
-  const time = () => {
-  const oneDay = 24*60*60*1000;
-  const oneHour = 1000 * 60 * 60;
-  const oneMinute = 1000 * 60;
-  const now = new Date();
-  const dayX = new Date(2017,05,25);
+  if ($('div').is('.timer')) {
+    const time = () => {
+    const oneDay = 24*60*60*1000;
+    const oneHour = 1000 * 60 * 60;
+    const oneMinute = 1000 * 60;
+    const now = new Date();
+    const dayX = new Date(2017,05,25);
 
-  const timeDiffInMillySeconds = dayX.getTime() - now.getTime();
+    const timeDiffInMillySeconds = dayX.getTime() - now.getTime();
 
-  const days = Math.floor(timeDiffInMillySeconds / oneDay);
-  const hours = Math.floor((timeDiffInMillySeconds - days*oneDay) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeDiffInMillySeconds - days*oneDay - hours*oneHour) / (1000 * 60));
+    const days = Math.floor(timeDiffInMillySeconds / oneDay);
+    const hours = Math.floor((timeDiffInMillySeconds - days*oneDay) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiffInMillySeconds - days*oneDay - hours*oneHour) / (1000 * 60));
 
-  const fullSeconds = timeDiffInMillySeconds - days*oneDay - hours*oneHour - minutes*oneMinute;
-  const seconds = Math.floor(fullSeconds / 1000);
+    const fullSeconds = timeDiffInMillySeconds - days*oneDay - hours*oneHour - minutes*oneMinute;
+    const seconds = Math.floor(fullSeconds / 1000);
 
-//  const template = days + ' ' + hours + ' ' + minutes + ' ' + seconds;
-  document.querySelector('.timer_days').textContent = days;
-  document.querySelector('.timer_hours').textContent = hours;
-  document.querySelector('.timer_minutes').textContent = minutes;
-}
-setInterval(time, 1000);
+  //  const template = days + ' ' + hours + ' ' + minutes + ' ' + seconds;
+
+      document.querySelector('.timer_days').textContent = days;
+      document.querySelector('.timer_hours').textContent = hours;
+      document.querySelector('.timer_minutes').textContent = minutes;
+
+
+  }
+  setInterval(time, 1000);
+  }
 })
---
+++ src/blocks/video/video.js
@@ -0,0 +1,29 @@
+$(function () {
+  $('.buttonbar').click (function () {
+       var video = $(this).before(".video");
+       var button = $(".play-button");
+       if (video.paused) {
+          video.play();
+          $(button).hide();
+       } else {
+          video.pause();
+          button.show();
+       }
+  })
+
+
+
+//
+//   var video = document.getElementsByClassName("video");
+//   window.setInterval(function(t){
+//     var video = document.getElementsByClassName("video");
+//     if (video.readyState > 0) {
+//       var duration = $('.video_time').get(0);
+//       var vid_duration = video.duration;
+//       var min = Math.floor(video.duration/60);
+//       var sec = Math.floor((video.duration - min*60)/60);
+//       console.log(min + ':' + sec);
+//       duration.firstChild.nodeValue = vid_duration;
+//       clearInterval(t);
+//     }
+//   },500);
