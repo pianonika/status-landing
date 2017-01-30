8db3b2923dc8c123a922ebd6eb5c9ab75ecc33a7
Kupriyanova Nataly <lozina@morzio.ru>
2017-01-30 10:35:37 +0300

    fonts

--- src/blocks/timer/timer.js
+++ src/blocks/timer/timer.js
@@ -1,6 +1,6 @@
 /*nataly*/
 $(function () {
-  if ($('div').is('.timer')) {
+  if ($('.timer').length > 0) {
     timer();
 
   setInterval(timer, 1000);
@@ -14,20 +14,24 @@ $(function () {
     const oneHour = 1000 * 60 * 60;
     const oneMinute = 1000 * 60;
     const now = new Date();
-    const dayX = new Date(2017,05,25);
+    const dayX = new Date(2017,02,01);
 
     const timeDiffInMillySeconds = dayX.getTime() - now.getTime();
 
-    const days = Math.floor(timeDiffInMillySeconds / oneDay);
-    const hours = Math.floor((timeDiffInMillySeconds - days*oneDay) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDiffInMillySeconds - days*oneDay - hours*oneHour) / (1000 * 60));
+    if (timeDiffInMillySeconds > 0) {
+      const days = Math.floor(timeDiffInMillySeconds / oneDay);
+      const hours = Math.floor((timeDiffInMillySeconds - days*oneDay) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDiffInMillySeconds - days*oneDay - hours*oneHour) / (1000 * 60));
 
-    const fullSeconds = timeDiffInMillySeconds - days*oneDay - hours*oneHour - minutes*oneMinute;
-    const seconds = Math.floor(fullSeconds / 1000);
+      const fullSeconds = timeDiffInMillySeconds - days*oneDay - hours*oneHour - minutes*oneMinute;
+      const seconds = Math.floor(fullSeconds / 1000);
 
-  //  const template = days + ' ' + hours + ' ' + minutes + ' ' + seconds;
+    //  const template = days + ' ' + hours + ' ' + minutes + ' ' + seconds;
 
-      document.querySelector('.timer_days').textContent = days;
-      document.querySelector('.timer_hours').textContent = hours;
-      document.querySelector('.timer_minutes').textContent = minutes;
+        document.querySelector('.timer_days').textContent = days;
+        document.querySelector('.timer_hours').textContent = hours;
+        document.querySelector('.timer_minutes').textContent = minutes;
+      } else {
+        $('.timer-wrap').html('<button type="submit" class="button">Войти</button>');
+      }
   }
